fd91baf658f723ebc38538bd10a259bbbcbe62fa
valentina.workmail@gmail.com <valentina.workmail@gmail.com>
2021-03-16 11:59:07 +0700

    fix fun

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -79,7 +79,12 @@ object Utils {
     }
 
     fun toInitials(firstName: String?, lastName: String?): String {
-        return "${if (firstName != "" && firstName != " " && firstName != null) firstName.getOrNull(0)?.toUpperCase() else null}${if (lastName != "" && lastName != " " && lastName != null) lastName.getOrNull(0)?.toUpperCase() else ""}"
+        val result1 = if (firstName != "" && firstName != " " && firstName != null) firstName.getOrNull(0)?.toUpperCase() else null
+        val result2 = if (lastName != "" && lastName != " " && lastName != null) lastName.getOrNull(0)?.toUpperCase() else null
+        var result:String?
+        result =
+            if(result1 == null && result2 == null) null else if(result1 != null && result2 != null) "$result1$result2" else if (result1 != null) "$result1" else "$result2"
+        return "$result"
     }
 }
 
